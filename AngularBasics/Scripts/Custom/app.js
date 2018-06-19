a19ff315db3e963e0e70d917cd3fdf885aff877f
Lee Owen <LeeMichaelO@outlook.com>
2018-06-19 06:15:46 -0700

    Added Scaffolding and restructured folders

--- AngularBasics/Scripts/Custom/app.js
+++ AngularBasics/Scripts/Custom/app.js
@@ -1,24 +1,2 @@
-﻿angular.module("todoListApp", [])
-    .controller("mainCtrl", function ($scope, dataService) {
-        $scope.learningNgChange = function () {
-            console.log("An input has changed!");
-        };
-
-        $scope.helloConsole = dataService.helloConsole;
-
-        dataService.getTodos(function (response) {
-            console.log(response.data);
-            $scope.todos = response.data;
-        });
-
-    })
-    .service('dataService', function ($http) {
-        this.helloConsole = function () {
-            console.log('This is the hello console service!');
-        }
-
-        this.getTodos = function (callback) {
-            $http.get('mock/todos.json')
-                .then(callback)
-        }
-    });
+﻿angular.module("todoListApp", []); 
+    
