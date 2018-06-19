ecc1f631f19b62d04c10bea7bdedf9f0ff649474
Lee Owen <LeeMichaelO@outlook.com>
2018-06-18 20:38:23 -0700

    Working with NG directives

--- AngularBasics/Scripts/Custom/app.js
+++ AngularBasics/Scripts/Custom/app.js
@@ -1,5 +1,5 @@
 ﻿angular.module("todoListApp", [])
-    .controller("mainCtrl", function($scope) {
+    .controller("mainCtrl", function ($scope) {
         $scope.helloWorld = function () {
             console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
         };
