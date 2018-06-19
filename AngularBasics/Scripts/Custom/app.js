7744a8f2dc867ff6195733b79ca94ca3178367e0
Lee Owen <LeeMichaelO@outlook.com>
2018-06-19 05:27:47 -0700

    Setting up services to work with mock data and load it onto the page

--- AngularBasics/Scripts/Custom/app.js
+++ AngularBasics/Scripts/Custom/app.js
@@ -1,16 +1,24 @@
 ﻿angular.module("todoListApp", [])
-    .controller("mainCtrl", function ($scope) {
-        $scope.helloWorld = function () {
-            console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
+    .controller("mainCtrl", function ($scope, dataService) {
+        $scope.learningNgChange = function () {
+            console.log("An input has changed!");
         };
 
-        $scope.todos = [
-            { "name": "Clean the House"},
-            { "name": "Water the Dog"},
-            { "name": "Feed the Lawn"},
-            { "name": "Pay dem Bills"},
-            { "name": "Run"},
-            { "name": "Swim"}
-        ]
+        $scope.helloConsole = dataService.helloConsole;
 
+        dataService.getTodos(function (response) {
+            console.log(response.data);
+            $scope.todos = response.data;
+        });
+
+    })
+    .service('dataService', function ($http) {
+        this.helloConsole = function () {
+            console.log('This is the hello console service!');
+        }
+
+        this.getTodos = function (callback) {
+            $http.get('mock/todos.json')
+                .then(callback)
+        }
     });
