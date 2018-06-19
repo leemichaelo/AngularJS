432597fc19ccf7a447a544982caaf79f1a6872f1
Lee Owen <LeeMichaelO@outlook.com>
2018-06-19 05:03:51 -0700

    Added ng-change to the edit button

--- AngularBasics/Scripts/Custom/app.js
+++ AngularBasics/Scripts/Custom/app.js
@@ -3,4 +3,14 @@
         $scope.helloWorld = function () {
             console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
         };
+
+        $scope.todos = [
+            { "name": "Clean the House"},
+            { "name": "Water the Dog"},
+            { "name": "Feed the Lawn"},
+            { "name": "Pay dem Bills"},
+            { "name": "Run"},
+            { "name": "Swim"}
+        ]
+
     });
